c12a8f023318116ac751f3291b8f82da59998572
BiagioCaputo <scebiagio98@gmail.com>
2024-01-23 14:26:14 +0100

    inserimento data e preparazione vue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 console.log("Vue ok", Vue)
+
+const {createApp} = Vue;
+
+const app = createApp({
+    name:"Vue tasks list",
+    data: () => ({
+        tasks,
+    }),
+
+    computed: {
+      
+    },
+
+    methods: {
+      
+    }
+
+});
+
+app.mount('#root');
